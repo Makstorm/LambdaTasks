55114c99c512762e3378dd238e47f7f525466afc
Makstorm <mxm.horobets@gmail.com>
2024-02-26 22:59:09 +0200

    feat: navigation feature interaction

--- 05_TelegramBot_Weather_Forecast/src/utils/bot-api.ts
+++ 05_TelegramBot_Weather_Forecast/src/utils/bot-api.ts
@@ -9,134 +9,84 @@ interface UserState {
   option?: number;
 }
 
-interface CustomKeyboard extends ReplyKeyboardMarkup {
-  reply_markup: {
-    keyboard: string[][];
-    resize_keyboard: boolean;
-    one_time_keyboard: boolean;
-  };
-}
-
 export const setupBotListeners = (bot: TelegramBot) => {
-  // const chatStates: Record<number, UserState> = {};
-  // const setUserState = (userId: number, state: UserState): void => {
-  //   chatStates[userId] = state;
-  // };
-  // const getUserState = (userId: number): UserState => {
-  //   return chatStates[userId] || { state: "" };
-  // };
-  // const stateHandlers: Record<
-  //   string,
-  //   (msg: TelegramBot.Message) => Promise<void>
-  // > = {
-  //   start: async (msg) => {
-  //     const chatId = msg.chat.id;
-  //     setUserState(chatId, { state: "fork" });
-  //     bot.sendMessage(chatId, "Вітаю, оберіть необхідний функціонал:", {
-  //       reply_markup: {
-  //         keyboard: [[{ text: "Курс валют" }, { text: "Прогноз погоди" }]],
-  //         one_time_keyboard: true,
-  //         resize_keyboard: true,
-  //       },
-  //     });
-  //   },
-  //   fork: async (msg) => {
-  //     const chatId = msg.chat.id;
-  //     if (msg.text.trim() === "Курс валют") {
-  //       //something with currency
-  //     } else if (msg.text.trim() === "Прогноз погоди") {
-  //       await stateHandlers["weaher_start"](msg);
-  //     } else {
-  //       bot.sendMessage(
-  //         chatId,
-  //         "Нажаль, такої опції, я поки не підтримую оберіть із представлених на кнопках: "
-  //       );
-  //       await stateHandlers["start"](msg);
-  //     }
-  //   },
-  //   weaher_start: async (msg) => {
-  //     const chatId = msg.chat.id;
-  //     setUserState(chatId, { state: "awaiting_option" });
-  //     bot.sendMessage(chatId, "Виберіть інтервал для відображення прогнозу:", {
-  //       reply_markup: {
-  //         keyboard: [[{ text: "3" }, { text: "6" }]],
-  //         one_time_keyboard: true,
-  //         resize_keyboard: true,
-  //       },
-  //     });
-  //   },
-  //   awaiting_option: async (msg) => {
-  //     const chatId = msg.chat.id;
-  //     const option = msg.text || "";
-  //     setUserState(chatId, { state: "awaiting_city", option: +option });
-  //     bot.sendMessage(
-  //       chatId,
-  //       `Ви обрали інтервал в ${option} год. Тепер, будь ласка введіть назву міста, прогноз в якому ви бажаєте отримати:`
-  //     );
-  //   },
-  //   awaiting_city: async (msg) => {
-  //     const chatId = msg.chat.id;
-  //     const city = msg.text || "";
-  //     const userState = getUserState(chatId);
-  //     const option = userState.option || 3;
-  //     bot.sendMessage(chatId, await getWeatherForecast(city, option));
-  //     setUserState(chatId, { state: "start" });
-  //     // await stateHandlers["start"](msg);
-  //   },
-  // };
-  // bot.onText(/\/weather/, async (msg) => {
-  //   await stateHandlers["weaher_start"](msg);
-  // });
-  // bot.onText(/\/start/, async (msg) => {
-  //   await stateHandlers["start"](msg);
-  // });
-  // bot.on("message", async (msg) => {
-  //   if (!msg.text) return;
-  //   const userState = getUserState(msg.chat.id);
-  //   if (userState.state) {
-  //     const handler = stateHandlers[userState.state];
-  //     if (handler) {
-  //       await handler(msg);
-  //       return;
-  //     }
-  //   }
-  //   if (msg.text.startsWith("/")) {
-  //     const command = msg.text.split(" ")[0];
-  //     switch (command) {
-  //       case "/start":
-  //       case "/weather":
-  //         break;
-  //       default:
-  //         bot.sendMessage(msg.chat.id, "Please use a valid command.");
-  //         break;
-  //     }
-  //   }
-  // });
+  const chatStates: Record<number, UserState> = {};
+  const setUserState = (userId: number, state: UserState): void => {
+    chatStates[userId] = state;
+  };
+  const getUserState = (userId: number): UserState => {
+    return chatStates[userId] || { state: "" };
+  };
+  const stateHandlers: Record<
+    string,
+    (msg: TelegramBot.Message, option?: number) => Promise<void>
+  > = {
+    awaiting_option: async (msg, option) => {
+      const chatId = msg.chat.id;
+
+      setUserState(chatId, { state: "awaiting_city", option: +option });
+      bot.sendMessage(
+        chatId,
+        `Ви обрали інтервал в ${option} год. Тепер, будь ласка введіть назву міста, прогноз в якому ви бажаєте отримати:`
+      );
+    },
+    awaiting_city: async (msg) => {
+      const chatId = msg.chat.id;
+      const city = msg.text || "";
+      const userState = getUserState(chatId);
+      const option = userState.option || 3;
+      bot.sendMessage(chatId, await getWeatherForecast(city, option));
+      setUserState(chatId, { state: "" });
+    },
+  };
+
+  bot.on("message", async (msg) => {
+    if (!msg.text) return;
+    const userState = getUserState(msg.chat.id);
+    if (userState.state) {
+      const handler = stateHandlers[userState.state];
+      if (handler) {
+        await handler(msg);
+        return;
+      }
+    }
+    if (msg.text.startsWith("/")) {
+      const command = msg.text.split(" ")[0];
+      switch (command) {
+        case "/start":
+        case "/weather":
+          break;
+        default:
+          bot.sendMessage(msg.chat.id, "Please use a valid command.");
+          break;
+      }
+    }
+  });
 
   const mainMenuKeyboard = {
     reply_markup: {
-      keyboard: [[{ text: "Feature 1" }, { text: "Feature 2" }]],
+      keyboard: [[{ text: "/Курс валют" }, { text: "/Погода" }]],
       resize_keyboard: true,
       one_time_keyboard: true,
     },
   };
 
-  const feature1Keyboard = {
+  const currencyOptions = {
     reply_markup: {
       keyboard: [
-        [{ text: "Option 1 - F2" }, { text: "Option 2 - F2" }],
-        [{ text: "Back to Menu" }],
+        [{ text: "USD" }, { text: "EUR" }],
+        [{ text: "Попереднє меню" }],
       ],
       resize_keyboard: true,
       one_time_keyboard: true,
     },
   };
 
-  const feature2Keyboard = {
+  const weatherForecastOptions = {
     reply_markup: {
       keyboard: [
-        [{ text: "Option 1 - F2" }, { text: "Option 2 - F2" }],
-        [{ text: "Back to Menu" }],
+        [{ text: "Кожні 3 години" }, { text: "Кожні 6 годин" }],
+        [{ text: "Попереднє меню" }],
       ],
       resize_keyboard: true,
       one_time_keyboard: true,
@@ -148,28 +98,42 @@ export const setupBotListeners = (bot: TelegramBot) => {
     bot.sendMessage(chatId, "Main Menu", mainMenuKeyboard);
   });
 
-  bot.on("message", (msg) => {
+  bot.onText(/\/Погода/, (msg) => {
+    const chatId = msg.chat.id;
+    bot.sendMessage(chatId, "Main Menu", weatherForecastOptions);
+  });
+
+  bot.onText(/\/Курс валют/, (msg) => {
+    const chatId = msg.chat.id;
+    bot.sendMessage(chatId, "Main Menu", currencyOptions);
+  });
+
+  bot.on("message", async (msg) => {
     const chatId = msg.chat.id;
     const text = msg.text || "";
 
     switch (text) {
-      case "Feature 1":
-        bot.sendMessage(chatId, "Feature 1 Options", feature1Keyboard);
+      case "/Погода":
         break;
-      case "Feature 2":
-        bot.sendMessage(chatId, "Feature 2 Options", feature2Keyboard);
+      case "/Курс валют":
         break;
-      case "Option 1 - F1":
-      case "Option 1 - F2":
-      case "Option 2 - F1":
-      case "Option 2 - F2":
-        bot.sendMessage(chatId, `You selected ${text}`);
+      case "USD": {
+        break;
+      }
+      case "EUR": {
+        break;
+      }
+
+      case "Кожні 3 години":
+        await stateHandlers["awaiting_option"](msg, 3);
+        break;
+      case "Кожні 6 годин":
+        await stateHandlers["awaiting_option"](msg, 6);
         break;
       case "Back to Menu":
         bot.sendMessage(chatId, "Main Menu", mainMenuKeyboard);
         break;
       default:
-        // Handling unknown command or navigating back to the main menu
         bot.sendMessage(
           chatId,
           "Please use a valid command.",
